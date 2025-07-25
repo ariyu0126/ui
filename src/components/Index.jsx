fd39cf11d63761aa0f50bb5816478a1310bf5815
ariyu <ariyu0126@gmail.com>
2025-07-25 14:12:23 +0900

    components 수정

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,4 +1,4 @@
 export { default as Button } from './Button';
-export { default as Input } from './Input/Index';
+export { default as Input } from './Input';
 export { default as SourceCodeViewer } from './SourceCodeViewer';
-export { default as Typography } from './Typography/Index';
+export { default as Typography } from './Typography';

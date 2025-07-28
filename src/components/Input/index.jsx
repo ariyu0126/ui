c10879272f178349114cdb7d4b0e0131a34203d7
ariyu <ariyu0126@gmail.com>
2025-07-28 09:49:05 +0900

    fix: rename to lowercase index.jsx for Netlify

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,16 @@
+import InputBase from './Input';
+import InputText from './InputText';
+import InputPassword from './InputPassword';
+import InputEmail from './InputEmail';
+import InputNumber from './InputNumber';
+import InputTel from './InputTel';
+
+const Input = {
+    Text: InputText,
+    Password: InputPassword,
+    Email: InputEmail,
+    Number: InputNumber,
+    Tel: InputTel,
+}
+
+export default Input;

4da10d56e26c28313065a1c6b3679cf71413ec1c
ariyu <ariyu0126@gmail.com>
2025-07-28 09:48:18 +0900

    temp rename for case sensitivity

---
+++ src/components/Typography/__temp.jsx
@@ -0,0 +1,32 @@
+import Title from './Title';
+import Text from './Text';
+
+const Typography = ({
+        // typography group
+        className,
+        // title
+        level = 1,
+        title,
+        titleColor,
+        titleClass,
+        // text
+        text,
+        weight,
+        textClass,
+        textColor,
+        ptag,
+        size,
+        ...rest
+    }) => {
+    return (
+        <div className={`typography--layout ${className}`}>
+            {title && <Title level={level} title={title} titleClass={titleClass} titleColor={titleColor} {...rest}/>}   
+            {text && <Text weight={weight} text={text} textClass={textClass} textColor={textColor} ptag={ptag} size={size} {...rest} />}
+        </div>
+    )
+}
+
+Typography.Title = Title;
+Typography.Text = Text;
+
+export default Typography;
